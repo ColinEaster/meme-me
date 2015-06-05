daad5a2786c1060fa991b552e327d3ed39490953
Colin Easter <Colin@Colins-MacBook-Pro.local>
2015-06-05 16:29:55 -0400

    Added an image picker.  Doesn't do anything with the image yet.

--- TestingGroundForMemeMe/ViewController.swift
+++ TestingGroundForMemeMe/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imagePickerButton: UIToolbar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pickAnImage(sender: UIBarButtonItem) {
+        
+        let pickerController = UIImagePickerController()
+        self.presentViewController(pickerController, animated: true, completion: nil)
+        
+    }
+    
 }
 
