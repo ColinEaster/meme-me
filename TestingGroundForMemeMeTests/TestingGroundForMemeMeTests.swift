ed4e12a2191a3df4ed4083b4b05af8e406c8eb6d
Colin Easter <Colin@Colins-MacBook-Pro.local>
2015-06-05 16:09:19 -0400

    Initial Commit

---
+++ TestingGroundForMemeMeTests/TestingGroundForMemeMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestingGroundForMemeMeTests.swift
+//  TestingGroundForMemeMeTests
+//
+//  Created by Colin Easter on 6/5/15.
+//  Copyright (c) 2015 Colin Easter. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TestingGroundForMemeMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
